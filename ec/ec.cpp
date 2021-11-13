7d83ce0ee7ef05ce58076196a09240203718eda6
zy1982808968 <1982808968@qq.com>
2021-11-13 22:30:16 +0800

    string

--- ec/ec.cpp
+++ ec/ec.cpp
@@ -1,20 +1,60 @@
-﻿// ec.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+using namespace std;
+#define max 100
 
-#include <iostream>
+typedef struct
+{
+	char data[max];
+	int curlen;
+}SeqString;
+
+
+void Init_String(SeqString &s)//字符串的初始化
+{
+	s.curlen = 0;
+}
+
+
+int intString(SeqString &s, string x)//创建字符串
+{
+	int i;
+	for (i = 0; x[i] != '\0'; i++) {
+		s.data[i] = x[i];
+		s.curlen = i;
+	}
+	return 1;
+}
+
+int length(SeqString &s) //求字符串的长度
+{
+	int length;
+	length = s.curlen + 1;
+	return length;
+
+}
+
+
+
+
+void print(SeqString &s) //遍历字符串
+{
+	int i;
+	for (i = 0; i < s.curlen; i++)
+	{
+		cout << s.data[i] << " ";
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SeqString s;
+	Init_String(s);
+	string x = { 'a','b','c','d','e','f','g','h','i','j', };
+	intString(s, x);
+	print(s);
+	cout << endl;
+	cout << "字符串的长度为：" << length(s);
+
+
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
