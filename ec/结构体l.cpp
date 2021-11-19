c3eefa5b45a54d863912f72705a025f8b79f65ed
zy1982808968 <1982808968@qq.com>
2021-11-19 20:51:27 +0800

    结构体的使用

--- ec/结构体l.cpp
+++ ec/结构体l.cpp
@@ -14,14 +14,14 @@ struct student
 
 int main() 
 {
-	//在结构体定义时Struct时可以省略的
-	 struct student s1;//第一种定义方法
+	//在结构体创建时Struct时可以省略的
+	 struct student s1;//第一种创建方法
 	 student s4;
 	s1.name = "张三" ;
 	s1.age = 18;
 	s1.score = 100;
 	cout << "姓名： " << s1.name << " 年龄 ：  " << s1.age << "成绩：  " << s1.score << endl;
-	struct  student s2 = { "李四",15,80};//第二种定义方法
+	struct  student s2 = { "李四",15,80};//第二种创建方法
 	
 	cout << "姓名： " << s2.name << " 年龄 ：  " << s2.age << "成绩：  " << s2.score << endl;
 	s3.name = "王五";
